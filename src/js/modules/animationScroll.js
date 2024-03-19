f58e7477d2e8f05710992ea7b05c0dd82f7013a7
IrinaBreus <irabreus@gmail.com>
2024-03-19 16:52:47 +0300

    add building process

--- src/js/modules/animationScroll.js
+++ src/js/modules/animationScroll.js
@@ -4,19 +4,20 @@ function animationScroll (className) {
           parentHeight = parent.getBoundingClientRect().height;
 
     document.addEventListener('scroll', () => {
-        if (document.documentElement.scrollTop >= parentPos - parentHeight / 4) {
+        if (document.documentElement.scrollTop >= parentPos - parentHeight / 4 && 
+        document.documentElement.scrollTop <= parentPos + parentHeight / 2) {
 
             moveAnimation('.animation-center', 'center-up', 'center-up-rev');
             moveAnimation('.animation-up', 'bottom-top', 'bottom-top-rev');
             moveAnimation('.animation-right', 'right-left', 'right-left-rev');
             
-        } else if (document.documentElement.scrollTop < parentPos || document.documentElement.scrollTop >= parentPos + parentHeight) {
+        } else  {
 
             moveAnimation('.animation-center', 'center-up-rev', 'center-up');
             moveAnimation('.animation-up', 'bottom-top-rev', 'bottom-top');
             moveAnimation('.animation-right', 'right-left-rev', 'right-left');
 
-        }
+        } 
     })
 
     function moveAnimation(elemClass, addClass, removeClass) {
