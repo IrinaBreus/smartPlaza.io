32ade9f75ae55b9bbb05af7be3cb95aa891d375e
IrinaBreus <irabreus@gmail.com>
2024-05-06 13:06:01 +0300

    add construction adaptation

--- src/js/modules/animationScroll.js
+++ src/js/modules/animationScroll.js
@@ -3,23 +3,24 @@ function animationScroll (className) {
           parentPos = parent.offsetTop,
           parentHeight = parent.getBoundingClientRect().height;
 
-    document.addEventListener('scroll', () => {
-        if (document.documentElement.scrollTop >= parentPos - parentHeight / 2 && 
-        document.documentElement.scrollTop <= parentPos + parentHeight / 2) {
-
-            moveAnimation('.animation-center', 'center-up', 'center-up-rev');
-            moveAnimation('.animation-up', 'bottom-top', 'bottom-top-rev');
-            moveAnimation('.animation-right', 'right-left', 'right-left-rev');
-            moveAnimation('.animation-left', 'left-right', 'left-right-rev');
-            // console.log(parentPos, parentHeight);
-        } else  {
-
-            moveAnimation('.animation-center', 'center-up-rev', 'center-up');
-            moveAnimation('.animation-up', 'bottom-top-rev', 'bottom-top');
-            moveAnimation('.animation-right', 'right-left-rev', 'right-left');
-            moveAnimation('.animation-left', 'left-right-rev', 'left-right');
-
-        } 
+    window.addEventListener('resize', () => {
+        document.addEventListener('scroll', () => {
+            if (document.documentElement.scrollTop >= parentPos - parentHeight / 2 && 
+            document.documentElement.scrollTop <= parentPos + parentHeight / 2) {
+    
+                moveAnimation('.animation-center', 'center-up', 'center-up-rev');
+                moveAnimation('.animation-up', 'bottom-top', 'bottom-top-rev');
+                moveAnimation('.animation-right', 'right-left', 'right-left-rev');
+                moveAnimation('.animation-left', 'left-right', 'left-right-rev');
+            } else  {
+    
+                moveAnimation('.animation-center', 'center-up-rev', 'center-up');
+                moveAnimation('.animation-up', 'bottom-top-rev', 'bottom-top');
+                moveAnimation('.animation-right', 'right-left-rev', 'right-left');
+                moveAnimation('.animation-left', 'left-right-rev', 'left-right');
+    
+            } 
+        })
     })
 
     function moveAnimation(elemClass, addClass, removeClass) {
