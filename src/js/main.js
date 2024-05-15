8553cba5d94b2d76d98d13bfb5d558ab4dd7bbc3
IrinaBreus <irabreus@gmail.com>
2024-05-15 20:16:08 +0300

    add conference adaptation

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,7 @@ if (window.screen.availWidth > 992) {
 }
 animationScroll('.founder');
 animationScroll('.haracteristics');
-// animationScroll('.conferece-hall');
+animationScroll('.conferece-hall');
 // animationScroll('.located');
 
 });
